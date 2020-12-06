afc59a658470fc3ebab9dc7ff0d090613d08e1de
Shuib Abdillahi <shuibabdillahi@MacBook-Pro.local>
2020-12-06 13:04:39 +0000

    changing to single page site

---
+++ src/components/NewPages/PortraitPages.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Allimage.css'
+import '../layout.css'
+import PortraitImages from '../Portraits/PortraitImages'
+
+const PortraitPages = () => {
+    return (
+        <div>
+            <PortraitImages/>
+        </div>
+    )
+}
+
+
+export default PortraitPages
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,14 @@ import SEO from "../components/seo"
 import HeroSection from "../components/HeroSection/HeroSection"
 import Image from '../components/Image'
 import Footer from '../components/Footer/Footer'
+import PortraitPages from "../components/NewPages/PortraitPages"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeroSection/>
     <Image />
-    
+    <PortraitPages/>
   </Layout>
 )
 
