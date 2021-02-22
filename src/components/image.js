ef549fa96b95526dae7f61f8f9fcf2675a9db0bf
shuibcodes <abdullahshuib124@gmail.com>
2021-02-22 21:21:43 +0000

    travel images

--- src/components/image.js
+++ src/components/image.js
@@ -1,60 +1,30 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
 import "../components/Allimage.css"
-
+import image1 from '../images/IMG_20190918_155436.jpg'
+import image2 from '../images/MVIMG_20180912_164407.jpg'
+import image3 from '../images/MVIMG_20180912_164407.jpg'
 const Image = () => {
-  const data = useStaticQuery(graphql`
-  query  {
-    allFile
-    (filter: 
-        {extension: {regex: "/(jpg)|(png)|(jpeg)/"}, 
-        name: {nin: ["20200903160705_IMG_8724","20200831183253_IMG_8587",
+ 
+return(
+    <div className="image-container">
+            <div className="imageGrid">
+                <img src={image1}alt="one"className="imageItem"></img>
+                <img src={image2}alt="one"className="imageItem"></img>
+                <img src={image3}alt="one"className="imageItem"></img>
+                <img src={image1}alt="one"className="imageItem"></img>
+                <img src={image2}alt="one"className="imageItem"></img>
+                <img src={image3}alt="one"className="imageItem"></img>
+                <img src={image1}alt="one"className="imageItem"></img>
+                <img src={image2}alt="one"className="imageItem"></img>
+                <img src={image3}alt="one"className="imageItem"></img>
+                <img src={image1}alt="one"className="imageItem"></img>
       
-      "20200903162058_IMG_8769","20200613154842_IMG_5658 (3) (6)",
-      "20200223152136_IMG_4315 (1)","IMG_6792","IMG_6157","IMG_5722 (1)","20200223145632_IMG_4225 (3)"
-      
-      ]}}
+            </div>
+    </div>
         
-    ) {
-      edges {
-        node {
-          base
-          childImageSharp {
-            fluid {
-                       ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  }
-  
-  
-  `)
-     // map through images and return 
-   return(
-     <>
-       <div className="header">
-                <h3>Travel</h3>
-        </div>
-   <div className="image-container">
-          
-          <div className="image-grid">
-              {data.allFile.edges.map((image,key)=> (
-                  // adding properties
-                  <Img key={key}
-                  className="image-item"
-                  fluid={image.node.childImageSharp.fluid}
-                  alt={image.node.base.split(".")[0]}
-                  />
-              ))}
-          </div>
-          
-      </div>
-     
-     </>
-    
+
+
+        
        
    )
 
--- src/pages/Images.js
+++ src/pages/Images.js
@@ -4,13 +4,30 @@ import Layout from '../components/Layout'
 import SEO from "../components/seo"
 import Image from '../components/Image'
 import Context from "../components/TravelContext/Context"
-
+import "../components/Allimage.css"
+import image1 from '../images/IMG_20190918_155436.jpg'
+import image2 from '../images/MVIMG_20180912_164407.jpg'
+import image3 from '../images/MVIMG_20180912_164407.jpg'
 const Images = () => (
   <Layout>
     <SEO title="Images" />
     <div id="scroll" > 
     <Context/>
-    <Image />
+    <div className="image-containerr">
+            <div className="imageGridd">
+                <img src={image1}alt="one"className="image-item"></img>
+                <img src={image2}alt="one"className="image-item"></img>
+                <img src={image3}alt="one"className="image-item"></img>
+                <img src={image1}alt="one"className="image-item"></img>
+                <img src={image2}alt="one"className="image-item"></img>
+                <img src={image3}alt="one"className="image-item"></img>
+                <img src={image1}alt="one"className="image-item"></img>
+                <img src={image2}alt="one"className="image-item"></img>
+                <img src={image3}alt="one"className="image-item"></img>
+                {/* <img src={image1}alt="one"className="image-item"></img> */}
+      
+            </div>
+    </div>
     </div>
     
   </Layout>
