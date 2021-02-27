ae7cfd20d35ea3c51b5f7eebc8da0480ca55af99
shuibcodes <abdullahshuib124@gmail.com>
2021-02-27 14:38:18 +0000

    projects page

--- src/pages/Images.js
+++ src/pages/Images.js
@@ -10,34 +10,37 @@ import image2 from '../images/IMG_6792.jpg'
 import image3 from '../images/20200903160705_IMG_8724.jpg'
 import Carousel from 'react-bootstrap/Carousel'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// import Tabs from 'react-bootstrap/Tabs'
 const Images = () => (
   <Layout>
     <SEO title="Images" />
     <div id="scroll" > 
     <Context/>
     <Carousel >
-<Carousel.Item interval={3000000} style={{width: "100%"}} >
+          <Carousel.Item interval={3000000}  >
 
-<img 
- className="d-block w-100"
- src={image1}
- alt="First slide"
-/>
-</Carousel.Item>
-<Carousel.Item interval={3000000} style={{width: "100%"}}>
-<img 
- className="d-block w-100"
- src={image2}
- alt="Second slide"
-/>
-</Carousel.Item>
-<Carousel.Item interval={1000000} style={{width: "100%"}} >
-<img
- className="d-block w-100"
- src={image3}
- alt="Third slide"
-/>
-</Carousel.Item>
+              <img
+              className="d-block w-100"
+              src={image1}
+              alt="First slide"
+          />
+          </Carousel.Item>
+          <Carousel.Item interval={3000000} style={{width: "100%"}}>
+              <img 
+              className="d-block w-100"
+              src={image2}
+              alt="Second slide"
+              />
+          </Carousel.Item>
+          <Carousel.Item interval={1000000} style={{width: "100%"}} >
+              <img
+              className="d-block w-100"
+              src={image3}
+              alt="Third slide"
+              />
+          </Carousel.Item>
 
 </Carousel>
     </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -10,14 +10,13 @@ import Carousel from 'react-bootstrap/Carousel'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-
 const Projects = () => (
     
   <Layout>
        <SEO title="Projects" />
 
        <Context/>
-       <div className="container">
+       <div className="album">
                     <Carousel indicators={true} controls={true} touch={true}  >
                     
                     <Carousel.Item  interval={3000000} style={{width: "100%"}} >
@@ -53,7 +52,43 @@ I’ve lived in England for 6 years, even though I don't visit Sudan as often as
 One thing I would say about us, is that Sudanese people preserve their values of generosity, kindness, and hospitality."</p>
                 </div>
        </div>
-      
+       <Context/>
+       <div className="album">
+                    <Carousel indicators={true} controls={true} touch={true}  >
+                    
+                    <Carousel.Item  interval={3000000} style={{width: "100%"}} >
+
+                    <img 
+                    className="d-block w-100"
+                    src={image1}
+                    alt="First slide"
+                    />
+                    </Carousel.Item>
+                    <Carousel.Item interval={3000000} style={{width: "100%"}}>
+                    <img 
+                    className="d-block w-100"
+                    src={image2}
+                    alt="Second slide"
+                    />
+                    </Carousel.Item>
+                    <Carousel.Item interval={1000000} style={{width: "100%"}} >
+                    <img
+                    className="d-block w-100"
+                    src={image3}
+                    alt="Third slide"
+                    />
+                    </Carousel.Item>
+
+                </Carousel>
+                <div className="album-description">
+                    <p className="desc-1" > "I’m Sudanese; both of my parents are Sudanese Nubians, from Sercmato and Dongola.
+I’ve lived in England for 6 years, even though I don't visit Sudan as often as I would like to, I’m very in touch with my culture and people back home. Just like London, Sudan as a whole is very diverse. This keeps me intrigued to learn more about both cultures. </p>
+<br></br>
+<br></br>
+<p className="desc-2">Today I’m wearing white representing a working Sudanese woman, and the facial marks are called “Shilukh” which were symbols of beauty as well as an identity marker in the 50-60s. Lines pattern differ depending on the tribe each is from.
+One thing I would say about us, is that Sudanese people preserve their values of generosity, kindness, and hospitality."</p>
+                </div>
+       </div>
   </Layout>
     
   
